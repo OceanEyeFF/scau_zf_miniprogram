c0b997f55d82e2e1d34d4ee776e01e72ceac8d87
OceanEyeFF <fdch00@163.com>
2021-07-25 02:15:29 +0800

    first commit

---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\Program Files (x86)\Tencent\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-8gp7gez3600bda0e --n quickstartFunctions --r --project "C:\Users\fdch0\WeChatProjects\miniprogram-1" --report
